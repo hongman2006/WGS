66753947d12dbe9e188c1c879455cc64e25e1993
HongMan© <152957304+hongman2006@users.noreply.github.com>
2024-09-04 22:14:48 +0800

    Update and rename 05.filter.sh to pca.sh

---
+++ pca.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+filename=85-81.1_18.SNP.filtered.PASS.novariants.cluster.windows.wms.vcf.gz
+WD=/projects/whj-chenhm/pig/labdata/05vcfnew/
+WD6=/projects/whj-chenhm/pig/labdata/06vcf_pca/
+gcta64=/projects/whj-chenhm/bin/gcta_1.94.0beta/gcta64
+$gcta64 --thread-num 40 --make-grm --out ${WD6}${filename}.gcta --bfile  ${WD}${filename}  --autosome-num 18
+$gcta64 --thread-num 40 --grm ${WD6}${filename}.gcta  --pca 20 --out ${WD6}${filename}.gcta &
+plink  --chr-set 18  --noweb --bfile  ${WD}${filename}   --pca 20 --genome --out  ${WD6}${filename}
+
+perl vcf_pca_tree.pl
+###
+filename=DNBN.1_18.SNP.filtered.PASS.novariants.cluster.windows.wms.vcf.gz
+WD=/projects/whj-chenhm/pig/labdata/06vcf_diannan_banna/
+WD6=/projects/whj-chenhm/pig/labdata/06vcf_pca/
+gcta64=/projects/whj-chenhm/bin/gcta_1.94.0beta/gcta64
+
+vcftools --gzvcf ${WD}${filename}  --plink --out ${WD}${filename} 
+plink --noweb --file ${WD}${filename}  --make-bed --out ${WD}${filename} 
+$gcta64 --thread-num 40 --make-grm --out ${WD6}${filename}.gcta --bfile  ${WD}${filename}  --autosome-num 18
+$gcta64 --thread-num 40 --grm ${WD6}${filename}.gcta  --pca 20 --out ${WD6}${filename}.gcta &
+plink  --chr-set 18  --noweb --bfile  ${WD}${filename}   --pca 20 --genome --out  ${WD6}${filename}
+
+perl vcf_pca_tree.pl
