ce48da4b4747f3be57537e1987b2607224b27093
HongMan© <152957304+hongman2006@users.noreply.github.com>
2024-09-04 22:41:33 +0800

    Update gone.sh

--- gone.sh
+++ gone.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-filename=list.85-81.1_18.SNP.filtered.PASS.novariants.cluster.windows.wms.ld502502.vcf.gz
-for ex in `ls *.$filename.map | cut -d "." -f1 | grep -v A3 | grep -v BM`
+for ex in `ls *.list | sed "s|.list||g"`
 do
-bash  script_GONE.sh $ex.$filename 
+bash  script_GONE.sh $ex.$tail.thin50.vcf.gz 
 done
