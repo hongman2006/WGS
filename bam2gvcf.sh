440dfbf5b11bd91e1cf7e61fd7d48ed2c177ab47
HongMan© <152957304+hongman2006@users.noreply.github.com>
2024-09-04 10:29:14 +0800

    Add files via upload

---
+++ bam2gvcf.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+echo `date`
+start=`date +%s`
+REF=/project-whj/software/pig.genome.refence/Sscrofa11.1/Sscrofa11.1_genomic.fa
+WD=/project-whj/zhuqy/20230814_bannajjx/38samples-NGS-data/
+WD2=${WD}41samples.cleandata/copy/dse/bam/
+WD3=${WD}03vcf/
+mkdir ${WD3}
+#DSE_CRR025077.sus_scrofa.merge.sort.dedup.bam
+
+tail=.sus_scrofa.merge.sort.dedup.bam
+cd ${WD3}
+cp ${WD}chr.list ${WD3}
+for sample in `ls ${WD2}*.sus_scrofa.merge.sort.dedup.bam | cut -d "/" -f10 | cut -d "." -f1`
+do
+mkdir ${sample} #注意替换样本名
+for SRA in `cat chr.list`
+do
+
+gatk  HaplotypeCaller --tmp-dir ./${sample}/ -R ${REF}  -I ${WD2}${sample}${tail} -O ./${sample}/${sample}.chr_${SRA}.Sscrofa11.1.sort.dedup.g.vcf.gz -ERC GVCF  -L ${SRA}  >${sample}.chr_${SRA}.log 2>&1 &
+done
+done
+end=`date +%s`
+echo "******gvcf done chm ****** TIME:`expr $end - $start`s"
+echo `date`
+
