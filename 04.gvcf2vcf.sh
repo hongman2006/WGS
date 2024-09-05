4f459d64261ccf38f899e9ad469c78c98eb33405
HongMan© <152957304+hongman2006@users.noreply.github.com>
2024-09-05 21:36:55 +0800

    Update 04.gvcf2vcf.sh

--- 04.gvcf2vcf.sh
+++ 04.gvcf2vcf.sh
@@ -36,18 +36,18 @@ REF=/projects/whj-chenhm/RNA-seq/pigtest/00ref/Sus_scrofa.Sscrofa11.1.dna.toplev
 gatk=/projects/software/anaconda3/share/gatk4-4.2.6.1-1/gatk-package-4.2.6.1-local.jar
 WD=/projects/whj-chenhm/pig/labdata/05vcfnew
 filename=SNP.filtered.PASS
-# 85.SNP.filtered.PASS.vcf.gz
+# 84.SNP.filtered.PASS.vcf.gz
 TMPDIR=$WD/tmp
 mkdir $TMPDIR
-# tabix 85.${filename}.vcf.gz 
+# tabix 84.${filename}.vcf.gz 
 java -Xms80g -Xmx120g -Djava.io.tmpdir=$TMPDIR -jar $gatk SelectVariants --exclude-non-variants \
        -R ${REF} \
-       -V 85.${filename}.vcf.gz \
-       -O 85.${filename}.novariants.wms.vcf.gz &
+       -V 84.${filename}.vcf.gz \
+       -O 84.${filename}.novariants.wms.vcf.gz &
 
 java -Xms80g -Xmx120g -Djava.io.tmpdir=$TMPDIR -jar $gatk SelectVariants --exclude-non-variants \
        -R ${REF} \
-       -V 85.${filename}.vcf.gz \
-       -O 85-82.${filename}.novariants.wms.vcf.gz \
-       --sample-name 82sample.list 
+       -V 84.${filename}.vcf.gz \
+       -O 84-81.${filename}.novariants.wms.vcf.gz \
+       --sample-name 81sample.list 
 echo "extract finished: $(date) chenhongman"
